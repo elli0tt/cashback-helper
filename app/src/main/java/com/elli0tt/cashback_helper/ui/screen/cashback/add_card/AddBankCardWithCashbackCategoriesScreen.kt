e27415ac5cdaa8276c91e4cde07d2c7a6d8be726
elli0tt <ilya.pestrikov2@gmail.com>
2024-11-27 21:18:42 +0300

    AddBankCardScreen: radio button row is fully clickable

--- app/src/main/java/com/elli0tt/cashback_helper/ui/screen/cashback/add_card/AddBankCardWithCashbackCategoriesScreen.kt
+++ app/src/main/java/com/elli0tt/cashback_helper/ui/screen/cashback/add_card/AddBankCardWithCashbackCategoriesScreen.kt
@@ -4,7 +4,7 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,13 +12,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -29,12 +27,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -76,7 +71,10 @@ fun AddBankCardWithCashbackCategoriesScreen(
                 item {
                     Column(modifier = Modifier.selectableGroup()) {
                         availableBankCardsNames.forEachIndexed { index, bankCardName ->
-                            Row(verticalAlignment = Alignment.CenterVertically) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.clickable { viewModel.selectBankCard(index) }
+                            ) {
                                 RadioButton(
                                     selected = index == selectedBankCardIndex,
                                     onClick = { viewModel.selectBankCard(index) })
